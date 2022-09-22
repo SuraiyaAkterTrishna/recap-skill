8891f3b66601befcce4093ef587d5ac422abca8c
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-09-22 22:29:33 +0600

    string boolean numeric typeof parseInt parseFloat stringConcatination toFixed

--- dataTypes.js
+++ dataTypes.js
@@ -5,6 +5,38 @@ var fishPrice = 100;
 var fishPrice = "100";
 var isCute = true;
 
-console.log(typeof fishPrice);
-console.log(typeof isCute);
+// console.log(typeof fishPrice);
+// console.log(typeof isCute);
 
+var aquariumPrice = 1000;
+var standPrice = 500;
+var totalSetupPrice = aquariumPrice + standPrice ;
+
+var aquariumPrice = aquariumPrice + '1';
+// console.log(typeof aquariumPrice);
+
+var bangla = 50;
+var english = 90;
+var total = bangla + english;
+
+var rest = english - bangla;
+
+bangla = bangla += 10;
+// console.log(bangla);
+
+var modulu = english % bangla;
+// console.log(modulu);
+var value = parseInt(aquariumPrice);
+console.log(value);
+
+var school = "newTown";
+var schoolVari = "Girls";
+console.log(school + ' ' + schoolVari);
+
+var firstCount = 0.2;
+var secondCount = 0.3;
+var finalCount = firstCount + secondCount;
+console.log(finalCount);
+
+var count = .2+.3;
+console.log(count);
