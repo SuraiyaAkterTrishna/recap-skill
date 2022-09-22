f6261623631796a8aedfd4e104956137b7876f95
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-09-22 22:42:05 +0600

    concatination

--- dataTypes.js
+++ dataTypes.js
@@ -50,3 +50,7 @@ let foo = 42; // foo is now a number
 foo = "bar"; // foo is now a string
 foo = true; // foo is now a boolean
 console.log(typeof foo);
+
+foo = 42; // foo is a number
+const result = foo + "1"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
+console.log(result); // 421
