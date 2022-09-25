e76b30adf2d4adf207bc3bf28775b4d234801824
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-09-25 07:23:22 +0600

    javascript fundamentals

---
+++ fundamentals.js
@@ -0,0 +1,6 @@
+// array 
+var friendsAge = [ 11, 13, 17, ];
+var  muriChanacurFee = [ 34, 55, 221, 3 ];
+var nayikas = [ 'mahi', 'opu', 'sabana' ];
+var nayikasCount = nayikas.length;
+console.log(nayikasCount);
